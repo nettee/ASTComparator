930d32947c44e3d6f1f8e50312f97994fd06bd1b
William <nettee.liu@gmail.com>
2017-09-29 10:12:42 +0800

    refactor to be OO-ed

---
+++ src/main/java/me/nettee/astcomparator/Main.java
@@ -0,0 +1,9 @@
+package me.nettee.astcomparator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String path = "/home/william/projects/java/query.example/src/simple/extension/Graduate.java";
+        Ast ast = Ast.fromFile(path);
+    }
+}
