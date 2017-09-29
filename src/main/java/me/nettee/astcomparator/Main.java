4f57fa987794dec2f6374c0e273d5468ade1fefd
William <nettee.liu@gmail.com>
2017-09-29 16:26:30 +0800

    cross calculate similarity

--- src/main/java/me/nettee/astcomparator/Main.java
+++ src/main/java/me/nettee/astcomparator/Main.java
@@ -1,13 +1,37 @@
 package me.nettee.astcomparator;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
 
-    public static void main(String[] args) {
-        String path = "/home/william/projects/java/query.example/src/simple/extension/Graduate.java";
-        Ast ast1 = Ast.fromFile(path);
-        Ast ast2 = Ast.fromFile(path);
+    private static double getSimilarity(String path1, String path2) {
+        Ast ast1 = Ast.fromFile(path1);
+        Ast ast2 = Ast.fromFile(path2);
         double similarity = ast1.similarityTo(ast2);
-        System.out.println(similarity);
+        return similarity;
     }
-    
+
+    public static void main(String[] args) {
+        for (Map.Entry<String, String> e1 : PATHS.entrySet()) {
+            String key1 = e1.getKey();
+            String path1 = e1.getValue();
+            for (Map.Entry<String, String> e2 : PATHS.entrySet()) {
+                String key2 = e2.getKey();
+                String path2 = e2.getValue();
+                double similarity = getSimilarity(path1, path2);
+                System.out.printf("%s <-> %s: %.3f\n", key1, key2, similarity);
+            }
+        }
+    }
+
+    private static final HashMap<String, String> PATHS = new HashMap<String, String>() {
+        {
+            put("person", "/home/william/projects/java/query.example/src/simple/extension/Person.java");
+            put("student", "/home/william/projects/java/query.example/src/simple/extension/Student.java");
+            put("undergraduate", "/home/william/projects/java/query.example/src/simple/extension/UnderGraduate.java");
+            put("graduate", "/home/william/projects/java/query.example/src/simple/extension/Graduate.java");
+        }
+    };
 }
